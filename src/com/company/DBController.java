c9b03cee07c50bdba0fae6e23a34d59fd163f361
djb7559 <djb7559@rit.edu>
2020-10-13 14:26:13 -0400

    Connection basic

---
+++ src/com/company/DBController.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBController {
+  public void connect() {
+    Connection connection;
+    try {
+      connection =
+          DriverManager.getConnection(
+              "jdbc:postgresql:reddwarf.cs.rit.edu/databases/p320_18",
+              "p320_18",
+              "ieshoocaiDeipi0iev1v");
+    } catch (SQLException throwable) {
+      throwable.printStackTrace();
+      System.exit(0);
+    }
+  }
+}
