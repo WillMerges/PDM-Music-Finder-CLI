6ebe3d5ea3f23ba7e56b106165bc43caf3fd90c5
WillMerges <wdmerges@gmail.com>
2020-10-13 15:14:48 -0400

    Added list command

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,13 +16,31 @@ public class Main {
     public static void parseTokens() {
         String fst = tokens.get(0).toLowerCase().trim();
         if(fst.equals("play")) {
-            if(tokens.size() != 2) {
+            if (tokens.size() != 2) {
                 System.out.println("The play command must be in the form: play <song>");
                 return;
             } else {
                 String song = tokens.get(1);
                 // TODO play the song
-                System.out.println("Played song: "+song);
+                System.out.println("Played song: " + song);
+            }
+        } else if(fst.equals("list")) {
+            // can list albums or artists
+            if(tokens.size() != 3) {
+                System.out.println("The list command must be in the form: list <album | artist> <name>");
+                return;
+            } else {
+                String snd = tokens.get(1).toLowerCase().trim();
+                String third = tokens.get(2).toLowerCase().trim();
+                if(snd.equals("artist")) {
+                    System.out.println("Artist: "+third);
+                    // TODO list artists' albums/songs
+                } else if(snd.equals("album")){
+                    System.out.println("Album: "+third+" By: TODO"); // TODO get artist of song
+                    // TODO list songs in album
+                } else {
+                    System.out.println("The list command must be in the form: list <album | artist> <name>");
+                }
             }
         } else if(fst.equals("exit") || fst.equals("quit")) {
             exit = true;
