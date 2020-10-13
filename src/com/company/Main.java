4f74885737f304b56d5a81a7af323b2a36432ad7
WillMerges <wdmerges@gmail.com>
2020-10-13 14:42:07 -0400

    parser skeleton

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        parseInput();
     }
+
+    public static void parseToken(String token) {
+        token = token.toLowerCase().trim();
+        if(token.startsWith("play")) {
+            // do stuff, then more stuff
+        } else {
+            System.out.println("Unrecognized command!\n");
+        }
+    }
+
+    public static void parseInput() {
+        Scanner s = new Scanner(System.in);
+        String token = "";
+        char c = '\0';
+
+        boolean exit = false;
+        while(!exit) {
+            System.out.print("--> ");
+            token = s.nextLine();
+            parseToken(token);
+        }
+    }
+
 }
