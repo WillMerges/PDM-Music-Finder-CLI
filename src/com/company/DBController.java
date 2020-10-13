8340ec23f82e267d4d66d48a5143fb6d4e6d13fd
djb7559 <djb7559@rit.edu>
2020-10-13 14:53:54 -0400

    Driver error

--- src/com/company/DBController.java
+++ src/com/company/DBController.java
@@ -5,16 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBController {
-  public void connect() {
+  public static void main(String[] args) {
     Connection connection;
     try {
+      Class.forName("lib.postgresql-42.2.17.jar");
       connection =
           DriverManager.getConnection(
-              "//jdbc:postgresql://reddwarf.cs.rit.edu/databases/p320_18currentSchema=myschema",
+              "//jdbc:postgresql://reddwarf.cs.rit.edu/databases/p320_18?currentSchema=myschema",
               "p320_18",
               "ieshoocaiDeipi0iev1v");
 
-    } catch (SQLException throwable) {
+    } catch (SQLException | ClassNotFoundException throwable) {
       throwable.printStackTrace();
       System.exit(0);
     }
