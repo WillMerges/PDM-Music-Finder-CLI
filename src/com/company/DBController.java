41a78a44c65b81fb03f5b7650378eb70b65a3f98
DavidEkstrom <dse5266@glados.cs.rit.edu>
2020-10-21 23:54:25 -0400

    Add commands work

--- src/com/company/DBController.java
+++ src/com/company/DBController.java
@@ -161,11 +161,12 @@ public class DBController {
     try {
       Statement statement = connection.createStatement();
       ResultSet resultSet
-              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE Username = " + user);
+              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE \"Username\" = \'" + user +"\'");
+      resultSet.next();
       int cid = resultSet.getInt("cid");
       resultSet.close();
       PreparedStatement insertStatement =
-              connection.prepareStatement("INSERT INTO \"ConsistsOfSong\" VALUES ?, ?");
+              connection.prepareStatement("INSERT INTO \"ConsistsOfSong\"(sid, cid) VALUES(?, ?)");
       insertStatement.setObject(1, sid);
       insertStatement.setObject(2, cid);
       insertStatement.execute();
@@ -186,11 +187,12 @@ public class DBController {
     try {
       Statement statement = connection.createStatement();
       ResultSet resultSet
-              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE Username = " + user);
+              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE \"Username\" = \'" + user +"\'");
+      resultSet.next();
       int cid = resultSet.getInt("cid");
       resultSet.close();
       PreparedStatement insertStatement =
-              connection.prepareStatement("INSERT INTO \"ConsistsOfAlbum\" VALUES ?, ?");
+              connection.prepareStatement("INSERT INTO \"ConsistsOfAlbum\"(aid, cid) VALUES(?, ?)");
       insertStatement.setObject(1, aid);
       insertStatement.setObject(2, cid);
       insertStatement.execute();
@@ -211,11 +213,12 @@ public class DBController {
     try {
       Statement statement = connection.createStatement();
       ResultSet resultSet
-              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE Username = " + user);
+              = statement.executeQuery("SELECT cid FROM \"Collection\" WHERE \"Username\" = \'" + user +"\'");
+      resultSet.next();
       int cid = resultSet.getInt("cid");
       resultSet.close();
       PreparedStatement insertStatement =
-              connection.prepareStatement("INSERT INTO \"ConsistsOfArtist\" VALUES ?, ?");
+              connection.prepareStatement("INSERT INTO \"ConsistsOfArtist\"(ArID, cid) VALUES(?, ?)");
       insertStatement.setObject(1, arid);
       insertStatement.setObject(2, cid);
       insertStatement.execute();
