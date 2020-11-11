74106964eaf64e7bee84a9202a5cc407e418e630
WillMerges <wdmerges@gmail.com>
2020-11-10 22:51:57 -0500

    weighting properly

--- scripts/genre_score.py
+++ scripts/genre_score.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-
 import sys
+
+# for now, use a linear weighting (maybe make logistic?)
+def generate_time_weights(N):
+    ret = []
+    delta = 100.0 / N
+    curr = 100.0
+    for i in range(N):
+        ret.append(curr)
+        curr = curr - delta
+
+    return ret
+
 
 if len(sys.argv) != 2:
     print("run as: genre_score.py <filename>")
@@ -8,7 +19,8 @@ if len(sys.argv) != 2:
 
 filename = sys.argv[1]
 
-#values are dictionary w/ keys of timestamps and values of genres
+# values are a list of dictionaries
+#[dictionary w/ key = timestamp val = genre, [sum of weights, dictionary w/ key = genre val = weight]]
 artists = dict()
 
 file = open(filename)
@@ -25,7 +37,7 @@ while reading:
     # new artist
     if line.find(",") == -1:
         arid = int(line)
-        artists[arid] = dict()
+        artists[arid] = [dict(),[0.0,dict()]]
         current_artist = arid;
     # value row
     else:
@@ -36,8 +48,42 @@ while reading:
             print("First line was not an artist, error reading file.")
             exit()
         else:
-            artists[arid][timestamp] = genre
+            artists[arid][0][timestamp] = genre
 
 # at this point the whole file is read
+
+# add up the weights
+for key in artists.keys():
+    timestamp_list = list(artists[key][0].keys())
+    if len(timestamp_list) == 0:
+        continue
+
+    time_weight_list = generate_time_weights(len(timestamp_list))
+
+    i = 0
+    for timestamp in timestamp_list:
+        time_weight = time_weight_list[i]
+        genre = artists[key][0][timestamp]
+
+        if genre in artists[key][1][1]:
+            artists[key][1][1][genre] = artists[key][1][1][genre] + time_weight
+        else:
+            artists[key][1][1][genre] = float(time_weight)
+
+        artists[key][1][0] = artists[key][1][0] + float(time_weight) # add to the sum
+
+        i = i + 1
+
+# normalize the weights
+for key in artists.keys():
+    for genre in artists[key][1][1]:
+        artists[key][1][1][genre] = artists[key][1][1][genre] / artists[key][1][0]
+
+#for testing only
+for arid in artists:
+    print(str(arid)+":")
+    for genre in artists[arid][1][1]:
+        weight = artists[arid][1][1][genre]
+        print(genre+"  ---  "+str(weight))
 
 file.close()
