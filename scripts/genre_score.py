ed87dcaa932941c49ae7a1a0ac832bcbe7bcb675
WillMerges <wdmerges@gmail.com>
2020-11-12 14:34:14 -0500

    plotting done

--- scripts/genre_score.py
+++ scripts/genre_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 # TODO
 # for now, use a linear weighting (maybe make logistic?)
@@ -86,7 +87,7 @@ for key in artists.keys():
     for genre in artists[key][1][1]:
         artists[key][1][1][genre] = artists[key][1][1][genre] / artists[key][1][0]
 
-#for testing only
+# for TESTING only, print all artists
 for arid in artists:
    print(str(arid)+"  ---  "+artists[arid][2])
    for genre in artists[arid][1][1]:
@@ -96,20 +97,72 @@ for arid in artists:
 
 
 if len(sys.argv) == 3:
-    arid = int(sys.argv[2])
+    tid = int(sys.argv[2])
 else:
-    arid = int(input("'arid' of artist to compare against: "))
+    tid = int(input("'arid' of artist to compare against: "))
     print()
 
-print(str(arid)+"  ---  "+artists[arid][2])
-for genre in artists[arid][1][1]:
-    weight = artists[arid][1][1][genre]
+print(str(tid)+"  ---  "+artists[tid][2])
+for genre in artists[tid][1][1]:
+    weight = artists[tid][1][1][genre]
     print(genre+"  ---  "+str(weight))
     i = i + 1
 
-genres = artists[arid][1][1].keys()
-weights = artists[arid][1][1].values()
-fig = plt.figure(figsize = (10, 5))
-plt.bar(genres, weights, width=0.95, color='orange')
+# TESTING show histogram
+# genres = artists[arid][1][1].keys()
+# weights = artists[arid][1][1].values()
+# fig = plt.figure(figsize = (10, 5))
+# plt.bar(genres, weights, width=0.95, color='orange')
+# plt.show()
 
+
+# key of delta, value of arid
+deltas = dict()
+
+for arid in artists:
+    if arid == tid:
+        continue
+    delta = 0
+    for genre in artists[tid][1][1]:
+        if genre in artists[arid][1][1]:
+            delta = delta + abs(artists[tid][1][1][genre] - artists[arid][1][1][genre])
+        else: #penalize for not having that genre
+            delta = delta + artists[tid][1][1][genre]
+    deltas[delta] = arid
+
+# TESTING print deltas
+for delta in sorted(deltas.keys()):
+    print(str(deltas[delta])+"  --  "+str(delta))
+
+# graph bar graph
+x_lab = artists[tid][1][1].keys()
+x = np.arange(len(x_lab))
+y = artists[tid][1][1].values()
+y1 = []
+y2 = []
+d1 = sorted(deltas.keys())[0]
+d2 = sorted(deltas.keys())[1]
+a1 = deltas[d1]
+a2 = deltas[d2]
+for genre in x_lab:
+    if genre in artists[a1][1][1]:
+        y1.insert(0, artists[a1][1][1][genre])
+    else:
+        y1.insert(0, 0.0)
+
+    if genre in artists[a2][1][1]:
+        y2.insert(0, artists[a2][1][1][genre])
+    else:
+        y2.insert(0, 0.0)
+
+plt.style.use('bmh')
+ax = plt.subplot(111)
+w = 0.1
+plt.xticks(x, x_lab)
+plt.ylabel("Normalized Weight")
+plt.xlabel("Genre")
+ax.bar(x-w, y, width=w, align='center', label=str(tid))
+ax.bar(x, y1, width=w, align='center', label=str(a1)+" - delta: "+str(d1))
+ax.bar(x+w, y2, width=w, align='center', label=str(a2)+" - delta: "+str(d2))
+plt.legend(loc="upper left")
 plt.show()
